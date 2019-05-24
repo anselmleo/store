ed6a3ea845c0e31a8bf17b91174f23ec16d3704e
Ridwan Maja <majataiwo@outlook.com>
2019-05-24 17:35:52 +0100

    eloquent pivot relationships

--- app/User.php
+++ app/User.php
@@ -37,9 +37,13 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    
+
     public function tasks()
     {
         return $this->hasMany(Task::class);
+    }
+
+    public function cart() {
+        return $this->belongsToMany(Product::class, Cart::class);
     }
 }
